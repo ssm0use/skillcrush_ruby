4614f9ec7278421acb48950af5f1e3132275f92e
ssm0use <ssm0use@gmail.com>
2020-10-06 15:30:35 -0400

    My First Commit Yay

---
+++ names.rb
@@ -0,0 +1,13 @@
+message = 'hello, world!' # hit enter
+message.upcase #hit enter
+
+name = 'SKILLCRUSH' #hit enter
+name.downcase #hit enter
+
+name.length #hit enter
+
+user = gets.chomp
+puts user + ", you typed: "
+puts message
+puts "and also\n"
+puts name
